f7ca122d99fc5184d69f6143f62fa5bbe6c18d41
aulia octavia <auliaoctaviam28@gmail.com>
2022-01-11 23:25:37 +0700

    Initial commit

---
+++ app/src/main/java/com/smkn9semarang/smpn14semarang/Ekstrakulikuler.java
@@ -0,0 +1,68 @@
+package com.smkn9semarang.smpn14semarang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.hardware.lights.LightState;
+import android.os.Bundle;
+import android.view.View;
+
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ekstrakulikuler extends AppCompatActivity {
+
+    CardView cvPaskibra;
+    CardView cvRebana;
+    CardView cvSepakbola;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ekstrakulikuler);
+        cvPaskibra = findViewById(R.id.Paskibra);
+        cvRebana = findViewById(R.id.Rebana);
+        cvSepakbola = findViewById(R.id.Sepakbola);
+
+        SliderView sliderView=findViewById(R.id.imageSlider);
+
+        List<Integer> images=new ArrayList<>();
+        images.add(R.drawable.b);
+        images.add(R.drawable.c);
+        images.add(R.drawable.d);
+        images.add(R.drawable.e);
+
+        MyAdapter myAdapter=new MyAdapter(images);
+
+        sliderView.setSliderAdapter(myAdapter);
+        sliderView.setAutoCycle(true);
+
+        cvPaskibra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Ekstrakulikuler.this,Paskibra.class);
+                startActivity(intent);
+            }
+        });
+
+        cvRebana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Ekstrakulikuler.this,Rebana.class);
+                startActivity(intent);
+            }
+        });
+
+        cvSepakbola.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Ekstrakulikuler.this,Sepakbola.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
